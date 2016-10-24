340b17dc2276505850456e356809006b9f61be74
Davide Sanvito <dsanvito90@gmail.com>
2016-10-24 10:00:09 +0200

    Plotting script update

--- plot.py
+++ plot.py
@@ -19,11 +19,15 @@ def build_date(yymmdd, hhmmss):
 
 
 for filename in glob.glob('results/*.p'):
-    p = pickle.load(open(filename, 'r'))['params']
-    print p
-    date = build_date(p['trace_day'], str(p['trace_ts']))
-    thrpt_values[date, p['Q'], p['N'], p['hash_func'], p['key_func'], p['clock_rate']] = \
-    pickle.load(open(filename, 'r'))['samples']['thrpt']
+    with open(filename, "rb") as f:
+        try:
+            results = pickle.load(f)
+        except EOFError:
+            print 'Error while reading', filename
+            continue
+        p = results['params']
+        date = build_date(p['trace_day'], str(p['trace_ts']))
+        thrpt_values[date, p['Q'], p['N'], p['hash_func'], p['key_func'], p['clock_rate']] = results['samples']['thrpt']
     date_values.add(date)
     Q_values.add(p['Q'])
     N_values.add(p['N'])
@@ -36,6 +40,7 @@ for N in N_values:
         for clock_rate in clock_rate_values:
             for date in sorted(date_values):
                 plt.figure()
+                plt.grid(True)
                 for key_func in key_func_values:
                     # standard deviation = np.std()
                     # standard deviation of the mean = standard error = np.std()/sqrt(len)
@@ -54,7 +59,7 @@ for N in N_values:
                      max(Q_values) + 0.1 * (max(Q_values) - min(Q_values))])
                 plt.xticks(range(min(Q_values), max(Q_values) + 1, 1))
                 plt.xlabel('Q')
-                plt.ylim([0, 1])
+                plt.ylim([0, 1.05])
                 plt.ylabel('Throughput [%]')
                 plt.legend(loc='lower left')
                 plt.setp(plt.gca().get_legend().get_texts(), fontsize='small')
